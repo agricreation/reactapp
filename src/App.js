0c10141ca473d338ec2507b4a98188c30e3abb36
moovendhanagri <72656436+moovendhanagri@users.noreply.github.com>
2023-05-04 14:50:55 +0530

    Cards added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-
 import './App.css';
 import Nav from './comp/nav.jsx';
+import Postcard from './comp/postcards';
+import Trending from './comp/trendig';
+import {Router} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Trending />
+      <Postcard />
+      <Postcard />
     </div>
   );
 }
